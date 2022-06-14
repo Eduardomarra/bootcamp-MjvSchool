189ae88d4f6952f5a99f25e4013a675d65c25779
Eduardo Marra <eduardomarra@gmail.com>
2022-06-14 17:41:50 -0300

    week two components

---
+++ week-two/week-two-component/src/app/links-navigation/links-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-links-navigation',
+  templateUrl: './links-navigation.component.html',
+  styleUrls: ['./links-navigation.component.scss']
+})
+export class LinksNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
